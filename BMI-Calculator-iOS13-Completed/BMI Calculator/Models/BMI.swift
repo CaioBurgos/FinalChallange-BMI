8f986490106dc735a26fb01bc3f0b236c714ca46
CaioBurgos <caio_burgos@hotmail.com>
2023-10-20 16:41:20 -0300

    BMI Calculator
    Final Project

---
+++ BMI-Calculator-iOS13-Completed/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  BMI Calculator
+//
+//  Created by Caio Burgos on 20/10/2023.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
